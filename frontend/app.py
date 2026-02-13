4fbd7aa7bd07a9bb519086b00d489d46b8f60818
Samir Dahal <samirdahal877@gmail.com>
2026-02-13 08:16:29 +0545

    improve file

--- frontend/app.py
+++ frontend/app.py
@@ -1,8 +1,9 @@
+import os
+
 import requests
 import streamlit as st
 
-
-API_BASE_URL = "http://localhost:8000"
+API_BASE_URL = os.getenv("API_BASE_URL", "http://localhost:8000")
 
 
 st.set_page_config(
